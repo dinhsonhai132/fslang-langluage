1bf8db3de1aee3202758103744424d84e44e4012
dinhsonhai132 <dinhsonhai440@gmail.com>
2024-09-12 11:32:32 +0700

    shell.py

--- shell.py
+++ shell.py
@@ -1,72 +1,79 @@
+import random
+
+INT = "INT"
+PLUS = "PLUS"
+MINUS = "MINUS"
+DIV = "DIV"
+TIME = "TIME"
+DIGITS = "1234567890"
+
 class data_type:
-    def __init__(self, type, value):
+    def __init__(self, type ,value):
         self.type = type
         self.value = value
 
-
-NUM = "NUM"
-NONE = "NONE"
-PLUS = "PLUS"
-MINUS = "MINUS"
-PRINT = "PRINT"
-
-
-class lexer():
-    def __init__(self, string) -> None:
-        self.pos = 0
+class lexer:
+    def __init__(self, string):
         self.string = string
-        self.cur = self.string[self.pos]
-        self.toks = []
+        self.pos = 0
+        self.cur_tok = self.string[self.pos]
 
     def tok(self):
-        self.cur = self.string[self.pos]
-        if self.cur in "1234567890":
-            self.toks.append(data_type(PLUS, 0))
-            self.toks.append(data_type(NUM, eval(self.cur)))
-
+        toks = []
         while self.pos < len(self.string):
-            self.cur = self.string[self.pos]
+            if self.pos < len(self.string):
+                self.cur = self.string[self.pos]
+            else:
+                self.cur = None
             if self.cur == "+":
-                self.toks.append(data_type(PLUS, 0))
+                toks.append(data_type(PLUS, 0))
             elif self.cur == "-":
-                self.toks.append(data_type(MINUS, 0))
-            elif self.cur in "1234567890":
-                self.toks.append(data_type(NUM, eval(self.cur)))
+                toks.append(data_type(MINUS, 0))
+            elif self.cur == "/":
+                toks.append(data_type(DIV, 0))
+            elif self.cur == "*":
+                toks.append(data_type(TIME, 0))
+            elif self.cur in DIGITS:
+                number = ""
+                while self.pos < len(self.string) and self.string[self.pos] in DIGITS:
+                    number += self.string[self.pos]
+                    self.pos += 1
+                toks.append(data_type(INT, number))
+                self.pos -= 1
             self.pos += 1
-        self.toks.append(data_type(NONE, 0))
-        return self.toks
-
+            
+        return toks
 
 class parser:
-    def __init__(self, toks):
+    def __init__(self, toks) -> None:
         self.toks = toks
         self.tok_idx = 0
         self.cur_tok = self.toks[self.tok_idx]
-
-    def calculators(self):
-        operators = []
-        numbers = []
-
-        while self.tok_idx < len(self.toks) and self.cur_tok.type is not NONE:
+    
+    def caculator(self):
+        toks = []
+        while self.tok_idx < len(self.toks):
             self.cur_tok = self.toks[self.tok_idx]
-            if self.cur_tok.type == NUM:
-                numbers.append(self.cur_tok.value)
+            if self.cur_tok.type == INT:
+                toks.append(self.cur_tok.value)
             elif self.cur_tok.type == PLUS:
-                operators.append(self.cur_tok.type)
+                toks.append(self.cur_tok.type)
             elif self.cur_tok.type == MINUS:
-                operators.append(self.cur_tok.type)
-            self.tok_idx += 1
+                toks.append(self.cur_tok.type)    
+            elif self.cur_tok.type == TIME:
+                toks.append(self.cur_tok.type)
+            elif self.cur_tok.type == DIV:
+                toks.append(self.cur_tok.type)
 
-        result = 0
         idx = 0
-        while idx < len(operators):
-            if operators[idx] == PLUS:
-                result += numbers[idx]
-            elif operators[idx] == MINUS:
-                result -= numbers[idx]
-            idx += 1
-
+        cur_num = 0
+        result = 0
+        while idx < len(toks):
+            if toks[idx] in DIGITS:
+                cur_num = int(toks[idx])
+            elif toks[idx] == PLUS:
+                result += cur_num
+            elif toks[idx] == MINUS:
+                result -= cur_num
+        
         return result
-
-    def input_output(self):
-        pass
