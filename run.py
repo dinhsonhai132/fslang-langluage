75c4393c79df1133c899cd2ae94d3b4e71bc6c09
dinhsonhai132 <dinhsonhai440@gmail.com>
2024-09-12 11:32:50 +0700

    run.py

--- run.py
+++ run.py
@@ -1,18 +1,15 @@
-from shell import data_type
 from shell import lexer
 from shell import parser
+import random
+import datetime
 
-run = True
-print('''
-fslang 0.0.2 (tags/v0.0.2:de54cfe) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.''')
-while run:
-    player = input(">> ")
-    if player == "help":
-        print('go to website [https://fslang.html] for help')
+print('''fslang 0.0.1 (tags/v0.0.1de54cf5) [MSC v.1938 64 bit (AMD64)] on win32
+type "help" or access https://github.com/dinhsonhai132?tab=repositories for more information...''')
+now = datetime.datetime.now()
+while True:
+    print(now.strftime("[%Y-%m-%d %H:%M:%S]"))
+    player = input(">>> ")
     lexers = lexer(player)
-    tok = lexers.tok()
-    parsers = parser(tok)
-    calculators = parsers.calculators()
-    print(calculators)
-    continue
+    toks = lexers.tok()
+    parsers = parser(toks)
+    print(parsers.caculator())
